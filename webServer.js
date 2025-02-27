06ea1bae75ec224ece430f8e816be2d28ee1004d
Emil Johnson <ejohn217@charlotte.edu>
2025-02-27 16:45:27 -0500

    Update webServer.js

--- webServer.js
+++ webServer.js
@@ -122,3 +122,125 @@ app.use(express.static(__dirname));
 app.get("/", function (request, response) {
   response.send("Simple web server of files from" + __dirname)
 });
+
+/**
+ * Use express to handle argument passing in the URL. This .get will cause
+ * express to accept URLs with /test/<something> and return the something in
+ * request.params.p1.
+ * 
+ * If implement the get as follows:
+ * /test        - Returns the SchemaInfo object of the database in JSON format.
+ *                This is good for testing connectivity with MongoDB.
+ * /test/info   - Same as /test.
+ * /test/counts - Returns an object with the counts of the different collections
+ *                in JSON format.
+ */
+app.get("/test/:p1", function (request, response) {
+  // Express parses the ":p1" from the URL and returns it in the request.params
+  // objects.
+  console.log("/test called with param1 = ", request.params.p1);
+
+  const param = request.params.p1 || "info";
+
+  if (param === "info") {
+    // Fetch the SchemaInfo. There should only one of them. The query of {} will
+    // match it.
+    SchemaInfo.find({}, function (err, info) {
+      if (err) {
+        // Query returned an error. We pass it back to the browser with an
+        // Internal Service Error (500) error code.
+        console.error("Error in /user/info:", err);
+        response.status(500).send(JSON.stringify(err));
+        return;
+      }
+      if (info.length === 0) {
+        // Query didn't return an error but didn't find the SchemaInfo object -
+        // This is also an internal error return.
+        response.status(400).send("Missing SchemaInfo");
+        return;
+      }
+
+      // We got the object - return it in JSON format.
+      console.log("SchemaInfo", info[0]);
+      response.end(JSON.stringify(info[0]));
+    });
+  } else if (param === "counts") {
+    // In order to return the counts of all the collections we need to do an
+    // async call to each collections. That is tricky to do so we use the async
+    // package do the work. We put the collections into array and use async.each
+    // to do each .count() query.
+    const collections = [
+      { name: "user", collection: User },
+      { name: "photo", collection: Photo },
+      { name: "schemaInfo", collection: SchemaInfo },
+    ];
+    async.each(
+      collections,
+      function (col, done_callback) {
+        col.collection.countDocuments({}, function (err, count) {
+          col.count = count;
+          done_callback(err);
+        });
+      },
+      function (err) {
+        if (err) {
+          response.status(500).send(JSON.stringify(err));
+        } else {
+          const obj = {};
+          for (let i = 0; i < collections.length; i++) {
+            obj[collections[i].name] = collections[i].count;
+          }
+          response.end(JSON.stringify(obj));
+        }
+      }
+    );
+  } else {
+    // If we know understand the parameter we return a (Bad Parameter) (400)
+    // status.
+    response.status(400).send("Bad param " + param);
+  }
+});
+
+/**
+ * URL /user/list - Returns all the User objects.
+ */
+app.get("/user/list", function (request, response) {
+  User.find({}, {_id: 1, first_name: 1, last_name: 1}, function (err, users) {
+    if (err) {
+      // Query returned an error.
+      console.error("Error in /user/list", err);
+      response.status(500).send(JSON.stringify(err));
+      return;
+    }
+    if (users.length === 0) {
+      // Query did not return an error, but could not find SchemaInfo object.
+      response.status(400).send();
+      return;
+    }
+    response.end(JSON.stringify(users));
+  });
+});
+
+/**
+ * URL /user/:id - Returns the information for User (id).
+ */
+app.get("/user/:id", function (request, response) {
+  const id = request.params.id;
+  User.find({_id: {$eq: id}},{__v:0}, function (err, user) {
+    if (err) {
+      // Query returned an error. We pass it back to the browser with an
+      // Internal Service Error (500) error code.
+      console.error("Error in /user/:id", err);
+      response.status(500).send(JSON.stringify(err));
+      return;
+    }
+    if (user.length === 0) {
+      // Query didn't return an error but didn't find the SchemaInfo object -
+      // This is also an internal error return.
+      response.status(400).send();
+      return;
+    }
+    // We got the object - return it in JSON format.
+    response.end(JSON.stringify(user[0]));
+  });
+});
